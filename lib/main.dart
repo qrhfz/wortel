463d704b5359e3cd7ad2c9891fe4c57f2c34f361
Qori El-Hafizh <contact@qori.dev>
2022-03-28 22:27:34 +0700

    ui update

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Wortel',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.brown,
         ),
         home: const HomePage(),
       ),
