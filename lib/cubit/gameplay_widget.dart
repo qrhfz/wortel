bf27a3bcc98d21b0e97825ddccf01ee76a6eb113
Qori El-Hafizh <contact@qori.dev>
2022-04-04 14:43:45 +0700

    remove unused import

--- lib/cubit/gameplay_widget.dart
+++ lib/cubit/gameplay_widget.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wortel/cubit/word_cubit.dart';
 import 'package:wortel/word_grid.dart';
 
 import '../keyboard.dart';
